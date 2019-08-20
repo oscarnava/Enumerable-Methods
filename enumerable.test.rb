9783b4fb25611955d305d8e33e5d93d7466b7c9a
oscarnava <oscarnava@gmail.com>
2019-08-19 19:33:35 -0500

    Functional versions of methods; only my_inject left

--- enumerable.test.rb
+++ enumerable.test.rb
@@ -1,14 +1,75 @@
 # frozen_string_literal: true
 
 require './my_enumerable.rb'
+require 'test/unit'
+extend Test::Unit::Assertions
 
-TEST_ARRAY = [1,2,3,4,5]
-TEST_HASH  = { one: 1, two: 2, three: 3, four: 4, five: 5 }
+def array_tests test_array
+  print 'Testing array: ', test_array, "\n"
 
-puts 'Testing array:'
-TEST_ARRAY.my_each { |n| puts n }
+  enum = test_array.each
+  test_array.my_each { |parm| assert_equal parm, enum.next }
 
-puts "\nTesting hash:"
-TEST_HASH.my_each { |key, value| puts "#{key} => #{value}" }
+  enum = test_array.each_with_index
+  test_array.my_each_with_index { |*parms| assert_equal parms, enum.next }
 
-# puts TEST_ARRAY.my_map { |n| n * n }
+  assert_equal(test_array.my_select(&:even?), test_array.select(&:even?))
+  assert_equal(test_array.my_select { |n| n > 5 }, test_array.select { |n| n > 5 })
+
+  assert_equal(test_array.my_all?(&:positive?), test_array.all?(&:positive?))
+  assert_equal(test_array.my_all? { |n| n > 7 }, test_array.all? { |n| n > 7 })
+
+  assert_equal(test_array.my_any?(&:positive?), test_array.any?(&:positive?))
+  assert_equal(test_array.my_any?(&:negative?), test_array.any?(&:negative?))
+
+  assert_equal(test_array.my_none?(&:negative?), test_array.none?(&:negative?))
+  assert_equal(test_array.my_none? { |n| n > 7 }, test_array.none? { |n| n > 7 })
+
+  assert_equal(test_array.my_count, test_array.count)
+  assert_equal(test_array.my_count(5), test_array.count(5))
+  assert_equal(test_array.my_count(50), test_array.count(50))
+  assert_equal(test_array.my_count(&:even?), test_array.count(&:even?))
+
+  my_array_proc = proc { |n| 7.0 / n }
+
+  assert_equal(test_array.my_map { |n| n**n }, test_array.map { |n| n**n })
+  assert_equal(test_array.my_map(my_array_proc), test_array.map(&my_array_proc))
+end
+
+def hash_tests test_hash
+  print 'Testing hash: ', test_hash, "\n"
+
+  enum = test_hash.each
+  test_hash.my_each { |*parms| assert_equal parms, enum.next }
+
+  enum = test_hash.each_with_index
+  test_hash.my_each_with_index { |*parms, idx| assert_equal [parms, idx], enum.next }
+
+  assert_equal(test_hash.my_select { |_, val| val.odd? }, test_hash.select { |_, val| val.odd? })
+
+  assert_equal(test_hash.my_all? { |_, val| val.positive? }, test_hash.all? { |_, val| val.positive? })
+  assert_equal(test_hash.my_all? { |_, val| val > 7 }, test_hash.all? { |_, val| val > 7 })
+
+  assert_equal(test_hash.my_any? { |_, val| val.positive? }, test_hash.any? { |_, val| val.positive? })
+  assert_equal(test_hash.my_any? { |_, val| val.even? }, test_hash.any? { |_, val| val.even? })
+
+  assert_equal(test_hash.my_none? { |_, val| val.negative? }, test_hash.none? { |_, val| val.negative? })
+  assert_equal(test_hash.my_none? { |_, val| val.odd? }, test_hash.none? { |_, val| val.odd? })
+
+  assert_equal(test_hash.my_count, test_hash.count)
+
+  assert_equal(test_hash.my_count(5), test_hash.count(5))
+  assert_equal(test_hash.my_count([:one, 1]), test_hash.count([:one, 1]))
+  assert_equal(test_hash.my_count { |_, n| n.even? }, test_hash.count { |_, n| n.even? })
+
+  my_hash_proc = proc { |key, n| key.object_id / n }
+
+  assert_equal(test_hash.my_map { |key, n| key.object_id * n }, test_hash.map { |key, n| key.object_id * n })
+  assert_equal(test_hash.my_map(my_hash_proc), test_hash.map(&my_hash_proc))
+end
+
+array_tests [1,2,3,4,5,6,7,8,9,10]
+array_tests [1,10,3,4,2,7,8,9,5,6]
+
+hash_tests one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, ten: 10
+hash_tests one: 10, six: 5, seven: 4, nine: 2, ten: 1, zero: 99, eight: 3, four: 7, five: 6, two: 9, three: 8
--- my_enumerable.rb
+++ my_enumerable.rb
@@ -9,31 +9,52 @@ module Enumerable
   end
 
   def my_each_with_index
-    # TODO: Complete function
+    idx = -1
+    my_each { |*vals| yield(*vals, idx += 1) }
   end
 
   def my_select
-    # TODO: Complete function
+    result = []
+    my_each do |*vals|
+      result.push(vals.size > 1 ? vals : vals.first) if yield(*vals)
+    end
+    return result.to_h if is_a? Hash
+
+    result
   end
 
   def my_all?
-    # TODO: Complete function
+    my_each do |*vals|
+      return false unless yield(*vals)
+    end
+    true
   end
 
   def my_any?
-    # TODO: Complete function
+    my_each do |*vals|
+      return true if yield(*vals)
+    end
+    false
   end
 
-  def my_none?
-    # TODO: Complete function
+  def my_none?(&block)
+    !my_any?(&block)
   end
 
-  def my_count
-    # TODO: Complete function
+  def my_count(value = nil, &block)
+    block = proc { |*val| value.nil? || value == val || val == [value] } unless block_given?
+    count = 0
+    my_each do |*vals|
+      count += 1 if block.call(*vals)
+    end
+    count
   end
 
-  def my_map
-    # TODO: Complete function
+  def my_map(proc = nil, &block)
+    block = proc if proc
+    result = []
+    my_each { |*vals| result.push block.call(*vals) }
+    result
   end
 
   def my_inject
